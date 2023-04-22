e5fb66d5b90a0a6931453afc57e7df006d43fbc2
Qi En HAN <57715857+hanqe8@users.noreply.github.com>
2023-04-23 00:57:14 +0800

    Create 182_Duplicate_Emails.sql

---
+++ SQL/182_Duplicate_Emails.sql
@@ -0,0 +1,26 @@
+-- Table: Person
+
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+
+-- Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+-- Return the result table in any order.
+-- The query result format is in the following example.
+
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+
+-- Solution
+Select Email
+from
+(Select Email, count(Email)
+from person
+group by Email having count(Email)>1) a
