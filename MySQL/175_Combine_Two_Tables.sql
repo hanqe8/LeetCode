55ec5433c92d20c254bf0d6ff948ca0ed99b00e7
Qi En HAN <57715857+hanqe8@users.noreply.github.com>
2023-04-22 15:46:14 +0800

    Update 175_Combine_Two_Tables.sql

--- MySQL/175_Combine_Two_Tables.sql
+++ MySQL/175_Combine_Two_Tables.sql
@@ -1,32 +1,31 @@
-Table: Person
+-- Table: Person
 
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| PersonId    | int     |
-| FirstName   | varchar |
-| LastName    | varchar |
-+-------------+---------+
-
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
 -- PersonId is the primary key column for this table.
 -- This table contains information about the ID of some persons and their first and last names.
 
-Table: Address
+-- Table: Address
 
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| AddressId   | int     |
-| PersonId    | int     |
-| City        | varchar |
-| State       | varchar |
-+-------------+---------+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
 -- addressId is the primary key column for this table.
 -- Each row of this table contains information about the city and state of one person with ID = PersonId.
 
-Write an SQL query to report the first name, last name, city, and state of each person in the Person table.
-If the address of a personId is not present in the Address table, report null instead.
-Return the result table in any order.
+-- Write an SQL query to report the first name, last name, city, and state of each person in the Person table.
+-- If the address of a personId is not present in the Address table, report null instead.
+-- Return the result table in any order.
 
 -- Solution
 select FirstName, LastName, City, State
