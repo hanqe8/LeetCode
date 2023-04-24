bb7ca1806edeef1dffd8859bb71e2ab197fea492
Qi En HAN <57715857+hanqe8@users.noreply.github.com>
2023-04-24 15:00:38 +0800

    Update 511_Game_Play_Analysis_I.sql

--- SQL/511_Game_Play_Analysis_I.sql
+++ SQL/511_Game_Play_Analysis_I.sql
@@ -42,6 +42,6 @@
 
 -- Solution
 
-Select player_id, min(event_date) as first_login
-from Activity
-group by player_id
+SELECT player_id, min(event_date) AS first_login
+FROM Activity
+GROUP BY player_id
