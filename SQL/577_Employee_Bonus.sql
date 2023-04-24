d33185fed3fd912fbb56d4e7f88ac1f776569c6c
Qi En HAN <57715857+hanqe8@users.noreply.github.com>
2023-04-24 15:01:03 +0800

    Update 577_Employee_Bonus.sql

--- SQL/577_Employee_Bonus.sql
+++ SQL/577_Employee_Bonus.sql
@@ -37,7 +37,7 @@
 
 -- Solution
 
-Select e.name, b.bonus
-from Employee e left join Bonus b
-on e.empId = b.empId
-where b.bonus < 1000 or b.bonus IS NULL
+SELECT e.name, b.bonus
+FROM Employee e LEFT JOIN Bonus b
+ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL
