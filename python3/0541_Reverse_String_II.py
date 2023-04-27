c697ace20c4959485fa9cf8ccb9402b1dd07c266
Qi En HAN <57715857+hanqe8@users.noreply.github.com>
2023-04-27 23:06:12 +0800

    Create 0541_Reverse_String_II.py

---
+++ python3/0541_Reverse_String_II.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        newS = ''
+        for i in range(0, len(s), 2*k):
+            newS += (s[i:i+k:][::-1]) + (s[i+k:i+(2*k)])
+        return newS
